f21ea0bbba160e80bf9361cf7ffca6224e3b56f7
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2022-03-30 22:45:08 +0000

    Add simple filter to index.html

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django import template
+user_model = get_user_model()
+
+register = template.Library()
+
+@register.filter
+def author_details(author):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    return name
